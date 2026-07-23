50b2e1daf7956354b430ebe1726b456c6e3422a7
rogerdv <rogerdv@gmail.com>
2026-07-23 05:49:37 -0400

    Attack system

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,14 +5,17 @@ public class PlayerController : MonoBehaviour
 {
     PlayerInput playerInput;
     public GameObject model;
+    public GameObject bulletPref;
 
     Animator anim;
+    Rigidbody body;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         playerInput = GetComponent<PlayerInput>();
         anim = GetComponent<Animator>();
+        body.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -24,10 +27,18 @@ public class PlayerController : MonoBehaviour
             Vector3 dir = new Vector3(input.x, 0, input.y);
             model.transform.rotation = Quaternion.Lerp(model.transform.rotation, Quaternion.LookRotation(dir, Vector3.up),0.5f);
             Vector3 mov = new Vector3(input.x, 0, input.y) * Time.deltaTime;
-            transform.Translate(mov * 2);
+            //body.MovePosition(body.position + mov);
+            transform.Translate(mov * 3.5f);
         } else {
             anim.SetFloat("Speed", 0);
         }
-        
+        if (playerInput.actions["Attack"].WasPressedThisFrame()) {
+            var projectile = Instantiate(bulletPref);
+            var offset = new Vector3(0, 1, 1);
+            projectile.GetComponent<Projectile>().dir= model.transform.forward;
+            
+            projectile.transform.position = model.transform.TransformPoint(offset);//model.transform.position +model.transform.forward *1.5f;
+            
+        }
     }
 }
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public Vector3 dir;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Vector3 dir;
+        transform.Translate(dir*10.0f*Time.deltaTime);
+        //transform.position = Vector3.MoveTowards(transform.position)
+    }
+}
